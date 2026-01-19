3aa9c0bd02fb8ffacc4c461970cbb561b95277d0
Raphael Bernardo <raphaelbgr@gmail.com>
2026-01-19 12:53:21 -0300

    Started implementing idling resource for espresso

--- app/src/androidTest/java/com/example/taller_kotlin_challenge/FixProductionCodeTest.kt
+++ app/src/androidTest/java/com/example/taller_kotlin_challenge/FixProductionCodeTest.kt
@@ -1,6 +1,7 @@
 package com.example.taller_kotlin_challenge
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -11,6 +12,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -31,9 +33,69 @@ class FixProductionCodeTest {
      */
     @Test
     fun invalidLoginShowsError() {
+
         onView(withId(R.id.username)).perform(typeText("bad"))
         onView(withId(R.id.password)).perform(typeText("creds"))
         onView(withId(R.id.bt_login)).perform(click())
         onView(withId(R.id.error_text)).check(matches(isDisplayed()))
+    }
+}
+
+/**
+ * Needs to implement Idling resouce
+ *
+ */
+object TestIdlingResource {
+
+    private const val RESOURCE = "GLOBAL"
+
+    @JvmField
+    val countingIdlingResource
+            = SimpleCountingIdlingResource(RESOURCE)
+
+    fun increment() {
+        countingIdlingResource.increment()
+    }
+
+    fun decrement() {
+        if (!countingIdlingResource.isIdleNow) {
+            countingIdlingResource.decrement()
+        }
+    }
+}
+
+class SimpleCountingIdlingResource(
+    private val resourceName: String
+) : IdlingResource {
+
+    private val counter = AtomicInteger(0)
+
+    @Volatile
+    private var resourceCallback:
+            IdlingResource.ResourceCallback? = null
+
+    override fun getName() = resourceName
+
+    override fun isIdleNow() = counter.get() == 0
+
+    override fun registerIdleTransitionCallback(
+        resourceCallback: IdlingResource.ResourceCallback
+    ) {
+        this.resourceCallback = resourceCallback
+    }
+
+    fun increment() {
+        counter.getAndIncrement()
+    }
+
+    fun decrement() {
+        val counterVal = counter.decrementAndGet()
+        if (counterVal == 0) {
+            resourceCallback?.onTransitionToIdle()
+        } else if (counterVal < 0) {
+            throw IllegalStateException(
+                "Counter has been corrupted!"
+            )
+        }
     }
 }
